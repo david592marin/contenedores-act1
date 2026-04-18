7bd9218397a27bca8d54b2f4d42fd73ff891721c
david592marin <david.marin592@comunidadunir.net>
2026-04-18 16:39:40 +0200

    act1 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://98.94.11.206:3001';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
